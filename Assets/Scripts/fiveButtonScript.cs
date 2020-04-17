d0a2ea6ac7e7e0f326071bd99214d73a2b870e84
kamotselane <kamogelotselane@gmail.com>
2020-04-17 16:33:04 +0200

    Pushing completed voting station scenes, might fix text later, still have more to fix in other scenes

---
+++ Assets/Scripts/fiveButtonScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fiveButtonScript : MonoBehaviour
+{
+    public GameObject one, two, three, four, five;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        one.gameObject.SetActive(false);
+        two.gameObject.SetActive(false);
+        three.gameObject.SetActive(false);
+        four.gameObject.SetActive(false);
+        five.gameObject.SetActive(false);
+
+    }
+    private bool isClicked = false;
+
+    private void OnMouseDown()
+    {
+        isClicked = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isClicked = false;
+    }
+    void Update()
+    {
+        if (isClicked)
+
+        {
+
+            one.gameObject.SetActive(false);
+            two.gameObject.SetActive(false);
+            three.gameObject.SetActive(false);
+            four.gameObject.SetActive(false);
+            five.gameObject.SetActive(true);
+        }
+    }
+}
